68a82b0f80639974fd432aa038b2e12abfbebf59
poojayadavsingh2-rgb <poojayadavsingh2@gmail.com>
2026-09-01 10:33:57 +0530

    Create table.c
    program of bca

---
+++ table.c
@@ -0,0 +1,24 @@
+/* table of 4*/
+# include<stdio.h>
+int main() {
+  int n;
+  int n1;
+  n=4;
+  n1=n*1;
+  printf("%d",n1);
+  n1=n*2;
+  printf("%d",n1);
+  n1=n*3;
+  printf("%d",n1);
+  n1=n*4;
+  printf("%d",n1);
+  n1=n*5;
+  printf("%d",n1);
+  n1=n*6;
+  printf("%d",n1);
+  n1=n*7;
+  printf("%d",n1);
+  return 0;
+}
+  
+
